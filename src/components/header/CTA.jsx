38e9c0b5551ab807b6649e5d7e93e0f7fb6f34d0
t-amandawong <amandamaenam@gmail.com>
2023-07-22 00:43:37 -0700

    fixed resizing issues with header

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,7 @@ import Resume from '../../assets/resume.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href={Resume} className='btn'>Download Resume</a>
+        <a href={Resume} className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk! </a>
     </div>
   )
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,11 +8,10 @@ const Header = () => {
   return (
     <header> 
         <div className="container header__container">
-          <h5>Hello, I'm</h5>
-            <h1>Thanyared Wong</h1>
-            <h5 className="text_light">Computer Science: Computer Game Design at UCSC</h5>
-            <CTA/>
-            <HeaderSocials/>
+          <h1>Thanyared Wong</h1>
+          <h5 className="text_light">Computer Science: Computer Game Design at UCSC</h5>
+          <CTA/>
+          <HeaderSocials/>
 
           <div className="me">
             <img src={ME} alt="Thanyared Wong smiling in front of a wall of leaves and flowers." />
