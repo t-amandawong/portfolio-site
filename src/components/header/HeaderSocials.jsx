f36d63795c44001ebabfe949198563fb4050433b
t-amandawong <amandamaenam@gmail.com>
2023-07-22 01:09:30 -0700

    fixed icon sizes

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import {BsGithub} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-        <a href="https://github.com/t-amandawong" target="_blank"><BsGithub/></a>
-        <a href="https://www.linkedin.com/in/thanyared/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/t-amandawong" target="_blank"><BsGithub size={32}/></a>
+        <a href="https://www.linkedin.com/in/thanyared/" target="_blank"><BsLinkedin size={32}/></a>
     </div>
   )
 }
