606cabab21e8b6ce91067dbedf816a4f6d471fdc
t-amandawong <amandamaenam@gmail.com>
2023-07-21 22:30:14 -0700

    added components to App.jsx and base style for css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Portfolio from './components/portfolio/Portfolio'
+import Experience from './components/experience/Experience'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Portfolio />
+      <Experience />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
