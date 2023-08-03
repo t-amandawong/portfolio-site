9fb4418c8e3eb04b91c747cabd497849dd520c60
t-amandawong <amandamaenam@gmail.com>
2023-08-03 12:12:37 -0700

    added more nav bar functionality

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import './nav.css'
 import {BiSolidHomeHeart} from 'react-icons/bi'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#"><BiSolidHomeHeart/></a>
-      <a href="#about"><BiSolidHomeHeart/></a>
-      <a href="#portfolio"><BiSolidHomeHeart/></a>
-      <a href="#experience"><BiSolidHomeHeart/></a>
-      <a href="#contact"><BiSolidHomeHeart/></a>
+      <a href="#" onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><BiSolidHomeHeart/></a>
+      <a href="#about" onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><BiSolidHomeHeart/></a>
+      <a href="#portfolio" onClick={()=> setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><BiSolidHomeHeart/></a>
+      <a href="#experience" onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiSolidHomeHeart/></a>
+      <a href="#contact" onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiSolidHomeHeart/></a>
     </nav>
   )
 }
