d25cfd337c850ba8ccea3f057b8ed35d36164546
t-amandawong <amandamaenam@gmail.com>
2023-08-02 22:18:48 -0700

    added navigation bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './nav.css'
+import {BiSolidHomeHeart} from 'react-icons/bi'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#"><BiSolidHomeHeart/></a>
+      <a href="#about"><BiSolidHomeHeart/></a>
+      <a href="#portfolio"><BiSolidHomeHeart/></a>
+      <a href="#experience"><BiSolidHomeHeart/></a>
+      <a href="#contact"><BiSolidHomeHeart/></a>
+    </nav>
   )
 }
 
